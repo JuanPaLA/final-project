7a87b4d1266b6210d9e8000959453809d9d79b11
Juan Pablo López Alurralde <jplopez@proyecto404.com>
2023-03-07 17:23:21 -0300

    first web commit

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,3 @@
+import { HomeScreen } from "@/src/ui/screens/HomeScreen";
+
+export default () => <HomeScreen />
---
+++ web/src/ui/screens/HomeScreen.tsx
@@ -0,0 +1,5 @@
+export const HomeScreen = () => (
+    <div>
+        <h1>Hola mundo</h1>
+    </div>
+)
